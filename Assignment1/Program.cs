3c794b0511b8fbaec61f74641f38e4cd7dbdc1f0
Sajidk25 <sajidk25@gmail.com>
2019-07-30 18:10:05 +0600

    basic array initialization added

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Assignment1.Math; 
+using Assignment1.Math;
 namespace Assignment1
 {
     class MainClass
@@ -10,12 +10,26 @@ namespace Assignment1
             sajid.FirstName = "Sajid";
             sajid.LastName = "khan";
             sajid.Introduce();
-
+            Console.WriteLine();
             //calculator class
             //var cal = new Calculator();
             // *** static implementation ***
             var result = Calculator.add(1, 2);
             Console.WriteLine(result);
+            //Array
+            Console.WriteLine();
+            int[] number = new int[3];
+            number[0] = 1;
+            Console.WriteLine(number[0]);
+            Console.WriteLine(number[1]);
+            Console.WriteLine(number[2]);
+            Console.WriteLine();
+            var flags = new bool[3];
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
         }
     }
 }
