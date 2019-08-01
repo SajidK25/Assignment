2de4ba15220c4af38135b119793066ead0db6105
Sajidk25 <sajidk25@gmail.com>
2019-08-01 10:06:40 +0600

    Value types and Reference type example added

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -2,6 +2,10 @@
 using Assignment1.Math;
 namespace Assignment1
 {
+    public class Man
+    {
+        public int age;
+    }
     class MainClass
     {
         public static void Main(string[] args)
@@ -47,6 +51,42 @@ namespace Assignment1
                         C:\folder2\folder3";
             Console.WriteLine(text2);
 
+            // *** Value types and Reference type example ***
+
+            // *** Value types example1 ***
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine("a :{0}, b:{1}",a,b);
+            // ***  Reference type example1 ***
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            Console.WriteLine("Before :: array1[0] :{0} , array1[1] :{1}", array1[0], array2[0]);
+            array2[0] = 0;
+            Console.WriteLine("After :: array1[0] :{0} , array1[1] :{1}", array1[0],array2[0]);
+
+            // *** Value types example2 ***
+            var num = 1;
+            // Increament methods argument 'num'  is different from var 'num'
+            Increment(num);
+            // after value incremented by 10 the argument 'num' is immediately destroyed
+            Console.WriteLine(num);// 1
+
+            // ***  Reference type example2 ***
+            var m = new Man() { age=20};
+            //Here agrument 'man' obejct is references same object 'man' in Heap
+            MakeOld(m);
+            Console.WriteLine(m.age);
+
+
+            void Increment(int n)
+            {
+                n += 10;
+            }
+            void MakeOld(Man man)
+            {
+                man.age += 10;
+            }
 
 
         }
