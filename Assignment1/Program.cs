9bfd169ba0213ae0002a75a5238289312467aa80
Sajidk25 <sajidk25@gmail.com>
2019-08-01 19:58:21 +0600

    default constructor, constructor overloading and this() initializer added

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -2,13 +2,20 @@
 using Assignment1.Math;
 namespace Assignment1
 {
-    
-    class MainClass
+
+	class MainClass
     {
         public static void Main(string[] args)
         {
 
-            
+			var customer = new Customer();
+			customer.Id = 1;
+			customer.Name = "john";
+
+			var order = new Order();
+			customer.Orders.Add(order);
+			Console.WriteLine(customer.Id);
+			Console.WriteLine(customer.Name );
 
         }
     }
