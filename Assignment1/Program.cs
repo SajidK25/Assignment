3543053f067cf7649537657a303aac4e7768f4a2
Sajidk25 <sajidk25@gmail.com>
2019-08-02 15:58:08 +0600

    Method overloading, null argument exception and handler global exception handeler implemented"

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -7,6 +7,19 @@ namespace Assignment1
     {
         public static void Main(string[] args)
         {
+			try {
+				var point = new Point(10, 20);
+				//point.Move(new Point(40, 60));
+				point.Move(null);
+				Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+
+				point.Move(400, 500);
+				Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
+			} catch (Exception ex) {
+				Console.WriteLine("An Unxpected error occured");
+			}
+
+			
 
 			var customer = new Customer();
 			customer.Id = 1;
