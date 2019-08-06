bed67966b1ac0cb4b51bcd11f0112dc8cad55f91
Sajidk25 <sajidk25@gmail.com>
2019-08-06 11:11:58 +0600

    DS169 completed

--- Assignment1/DS169.cs
+++ Assignment1/DS169.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections;
+
 namespace Assignment1
 {
 	public class DS169
 	{
 		public DS169()
 		{
+			
+		}
+		public static void SolutionDS169()
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i <= t; i++) {
+
+				var input = Console.ReadLine().Split(' ');
+				double v = double.Parse(input[0]);
+				double s = double.Parse(input[1]);
+
+				Console.WriteLine("{0:F2}", -(v / s));
+			}
 		}
 	}
 }
--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -7,37 +7,9 @@ namespace Assignment1
     {
         public static void Main(string[] args)
         {
-			try {
-				var point = new Point(10, 20);
-				//point.Move(new Point(40, 60));
-				point.Move(null);
-				Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
 
-				point.Move(400, 500);
-				Console.WriteLine("Point is at ({0},{1})", point.X, point.Y);
-			} catch (Exception ex) {
-				Console.WriteLine("An Unxpected error occured");
-			}
+			DS169.SolutionDS169();
 
-			
-
-			var customer = new Customer();
-			customer.Id = 1;
-			customer.Name = "john";
-
-			var order = new Order();
-			customer.Orders.Add(order);
-			Console.WriteLine(customer.Id);
-			Console.WriteLine(customer.Name );
-			// Object Initializer syntex
-			var anotherCustmer = new Customer() {
-				Id = 2,
-				Name = "Sajid"
-
-			};
-			var name = anotherCustmer.Name;
-			Console.WriteLine(name);
-
-        }
+		}
     }
 }
