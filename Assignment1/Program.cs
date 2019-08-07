3f250ec1377269f58ca3381ef59503d69204f4ba
Sajidk25 <sajidk25@gmail.com>
2019-08-07 11:31:21 +0600

    Array and List example added

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Assignment1.Math;
+using System.Collections.Generic;
 namespace Assignment1
 {
 
@@ -7,6 +8,74 @@ namespace Assignment1
     {
         public static void Main(string[] args)
         {
+			// ***************** List *********************
+			var numbers = new List<int> { 1, 2, 3, 4, 5 };
+			numbers.Add(1);
+			numbers.AddRange(new int[3] {6,7,8 });// When see IEnumrable interaface provide a instance of List or Array
+			foreach (var number in numbers) {
+				Console.WriteLine(number);
+			}
+			//Indexof
+			
+			Console.WriteLine("Index of 1 :"+ numbers.IndexOf(1));
+			Console.WriteLine("LastIndex of 1 :" + numbers.LastIndexOf(1));
+
+			//Count()
+			Console.WriteLine("Count :" + numbers.Count);
+
+			//Remove()
+			for (int i = 0; i < numbers.Count; i++) {
+				if (numbers[i] == 1) {
+					numbers.Remove(numbers[i]);
+
+				}
+			}
+			
+			foreach (var number in numbers) {
+				Console.WriteLine(number);
+			}
+			//Clear()
+			numbers.Clear();
+			Console.WriteLine("Count :" + numbers.Count);
+
+			/*
+			 // ***************** Arrays *********************
+			var numbers = new [] { 3, 7, 9, 2, 14, 6 };
+
+			//length
+			Console.WriteLine("Length : "+numbers.Length);
+			//IndexOf
+			var index = Array.IndexOf(numbers, 9);
+			Console.WriteLine(index);
+			//clear()
+			Array.Clear(numbers, 0, 2);
+			foreach (var n in numbers) {
+				Console.WriteLine(n);
+			}
+
+			var booleanArray = new[] { true, false, false, true };
+			var strArray = new[] { "sajid", "mollika", "raskin", "rahat", "riad" };
+
+			Array.Clear(booleanArray, 0,2);
+			Console.WriteLine("after clear booleanArray");
+			foreach (var ba in booleanArray) {
+				Console.WriteLine(ba);
+			}
+
+			Array.Clear(strArray, 0, 2);
+			Console.WriteLine("after clear strArray");
+			foreach (var sa in strArray) {
+				Console.WriteLine(sa);
+			}
+			Console.WriteLine("Length of strArray : "+strArray.Length);
+
+			//Copy()
+			var anotherNumbers = new int[4];
+			Array.Copy(numbers, anotherNumbers, 4);
+			Console.WriteLine("effect of Copy():");
+			foreach (var an in anotherNumbers) {
+				Console.WriteLine(an);
+			}
 			try {
 				var point = new Point(10, 20);
 				//point.Move(new Point(40, 60));
@@ -37,7 +106,8 @@ namespace Assignment1
 			};
 			var name = anotherCustmer.Name;
 			Console.WriteLine(name);
+			*/
 
-        }
-    }
+		}
+	}
 }
