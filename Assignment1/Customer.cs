fa3ca1d4d406d14907d7638148c17f2be18cfa1e
Sajidk25 <sajidk25@gmail.com>
2019-08-22 11:29:25 +0600

    First commit

--- Assignment1/Customer.cs
+++ Assignment1/Customer.cs
@@ -1,29 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace Assignment1
+﻿namespace Assignment1
 {
 	public class Customer
 	{
-		public int Id;
-		public string Name;
-		public List<Order> Orders;
 
-		public Customer()
-		{
-			Orders = new List<Order>();
-		}
-
-		public Customer(int id)
-			:this()
-		{
-			this.Id = id;
-		}
-
-		public Customer(int id,string name)
-			:this(id)
-		{
-			// this.Id = id;
-			this.Name = name;
-		}
 	}
 }
