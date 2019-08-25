716251102af066ef7972e311410434a0abedba0b
Sajidk25 <sajidk25@gmail.com>
2019-08-26 00:52:13 +0600

    property implemented

---
+++ Assignment1/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Assignment1
+{
+	public class Person
+	{
+		//******** Property get/set **********
+		//Auto implementation of private property
+		public DateTime BirthDate { get; private set; }
+		public Person(DateTime birthDate)
+		{
+			BirthDate = birthDate;
+		}
+		public int Age {
+			get {
+				var timeSpan = DateTime.Today - BirthDate;
+				var year = timeSpan.Days / 365;
+				return year;
+			}
+		}
+		/*
+		private DateTime _birthDate;
+		public DateTime BirthDate {
+			get { return _birthDate; }
+			set { _birthDate = value; }
+		}
+		*/
+
+		/******** getter/setter **********
+
+		private DateTime BirthDate;
+
+		public void SetBirthDate(DateTime birthDate)
+		{
+			BirthDate = birthDate;
+		}
+		public DateTime GetBirthDate()
+		{
+			return BirthDate;
+		}
+		*/
+
+	}
+}
--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -5,7 +5,15 @@ namespace Assignment1
     {
         public static void Main(string[] args)
         {
-
+			var person = new Person(new DateTime(1981, 01, 06));
+			//person.BirthDate = new DateTime(1981, 01, 06);
+			Console.WriteLine(person.Age);
+			/******** getter/setter **********
+			var person = new Person();
+			person.SetBirthDate(new DateTime(1985,01,06));
+			var dob=person.GetBirthDate();
+			Console.WriteLine(dob);
+			*/
 		}
 	}
 }
