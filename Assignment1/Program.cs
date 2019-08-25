30a8f53fa958b755c2ac062e4805eec0ac91ee11
Sajidk25 <sajidk25@gmail.com>
2019-08-25 11:44:32 +0600

    First Commit

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -6,15 +6,6 @@ namespace Assignment1
         public static void Main(string[] args)
         {
 
-			var customer = new Customer(1);
-			customer.Orders.Add(new Order());
-			customer.Orders.Add(new Order());
-			customer.Orders.Add(new Order());
-			customer.Orders.Add(new Order());
-
-			customer.Promote();
-			Console.WriteLine(customer.Orders.Count);
-
 		}
 	}
 }
