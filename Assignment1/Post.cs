05598f8e26d450f358f8f698a6e0fef81d0c6d50
Sajidk25 <sajidk25@gmail.com>
2019-08-30 17:47:27 +0600

    Persial commit

---
+++ Assignment1/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+	public class Post
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public readonly List<Votes> vote = new List<Votes>();
+	}
+}
---
+++ Assignment1/Votes.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Markup;
+
+namespace Assignment1
+{
+	public class Votes
+	{
+		private int _vote;
+
+		public int Vote {
+			get { return Vote; }
+			private set { _vote = value; }
+		}
+
+		public int upVote()
+		{
+			return _vote += 1;
+		}
+		public int downVote()
+		{
+			return _vote -= 1;
+		}
+	}
+}
